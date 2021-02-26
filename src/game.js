f28336ce0d66736f7fa5f413c0006e8f9988c2a6
Pavel Fokin <fokinpavel2007@outlook.com>
2021-02-26 22:53:44 +0300

    first commit

---
+++ src/game.js
@@ -0,0 +1,17 @@
+﻿import Charaster from './domian';
+
+export class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+export class GameSavingData {
+}
+
+export function readGameSaving() {
+}
+
+export function writeGameSaving() {
+}
+
